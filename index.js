d386acd5ca251fc6485fa54ee13cca4dc35908de
Azhor Ahai <debapriya.das.blr@gmail.com>
2017-09-16 22:09:59 +0530

    added prototype get post

---
+++ index.js
@@ -0,0 +1,17 @@
+var bodyParser = require('body-parser')
+var express = require('express')
+var app = express()
+var xhub = require('express-x-hub')
+
+app.set('port', (process.env.PORT || 5000))
+app.listen(app.get('port'))
+
+app.get('/', (req, res)=>{
+  console.log("GET here")
+  res.send("bar")
+})
+
+app.post('/', (req, res)=> {
+  console.log("POST here")
+  res.send("bar")
+})
