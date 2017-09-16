b8b68213f238d4f78f7e1f487af90339055ccd2e
Azhor Ahai <debapriya.das.blr@gmail.com>
2017-09-17 01:20:56 +0530

    facebook verify

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-var bodyParser = require('body-parser')
-var express = require('express')
-var app = express()
-var xhub = require('express-x-hub')
+const bodyParser = require('body-parser')
+, express = require('express')
+, app = express()
+, xhub = require('express-x-hub')
+, token = "bar"
 
 app.set('port', (process.env.PORT || 5000))
 app.listen(app.get('port'))
@@ -12,11 +13,17 @@ app.get('/', (req, res)=>{
   console.log(req.param('hub.verify_token'))
   console.log(req.params)
   console.log("GET here")
-  res.send("bar")
-})
+  if(req.param('hub.mode') == 'subscribe'
+  && req.param('hub.verify_token') == token) {
+    res.send(req.param('hub.challenge'))
+    }
+  })
 
 app.post('/', (req, res)=> {
   console.log("POST here")
   console.log(req.param)
-  res.send(req.param['hub.mode'])
+  if(req.param('hub.mode') == 'subscribe'
+  && req.param('hub.verify_token') == token) {
+    res.send(req.param('hub.challenge'))
+  }
 })
